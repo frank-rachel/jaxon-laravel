689995d9aa9d1058413342fa851ddd6cd967792f
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2025-04-19 19:53:45 +0300

    Create Jaxon.php

---
+++ src/Jaxon.php
@@ -0,0 +1,10 @@
+<?php
+namespace Jaxon\Laravel;
+
+/**
+ * Back‑compat shim for code that expects Jaxon\Laravel\Jaxon
+ */
+class Jaxon extends \Jaxon\Laravel\App\Jaxon
+{
+    // nothing here – we only inherit the facade
+}
