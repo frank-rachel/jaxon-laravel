d678e2ef5de96f90895588b25014eb11e12a2652
frank-rachel <73958149+frank-rachel@users.noreply.github.com>
2025-04-25 20:41:48 +0300

    b

--- src/App/Jaxon.php
+++ src/App/Jaxon.php
@@ -3,44 +3,49 @@ namespace Jaxon\Laravel\App;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Log;
-use Jaxon\App\App;           // The new base class
-use Jaxon\Di\Container;
+use Jaxon\App\Ajax as AjaxApp; // The old base class in jaxon-core < 4.8
 use Jaxon\Exception\SetupException;
+
 use function asset;
 use function config;
 use function public_path;
-use function response;
 use function route;
+use function response;
 
-class Jaxon extends App
+class Jaxon extends AjaxApp
 {
-    public function __construct()
+    /**
+     * Configure Jaxon for the Laravel runtime.
+     *
+     * @param string $sConfig
+     *
+     * @return void
+     * @throws SetupException
+     */
+    public function setup(string $sConfig = ''): void
     {
-        // Pass this instance to Jaxon’s DI container
-        parent::__construct(new Container($this));
-    }
+        // Blade directives
+        Blade::directive('jxnHtml', fn($e) => '<?php echo Jaxon\\attr()->html(' .$e.'); ?>');
+        Blade::directive('jxnBind', fn($e) => '<?php echo Jaxon\\attr()->bind(' .$e.'); ?>');
+        Blade::directive('jxnPagination', fn($e) => '<?php echo Jaxon\\attr()->pagination(' .$e.'); ?>');
+        Blade::directive('jxnOn', fn($e) => '<?php echo Jaxon\\attr()->on(' .$e.'); ?>');
+        Blade::directive('jxnClick', fn($e) => '<?php echo Jaxon\\attr()->click(' .$e.'); ?>');
+        Blade::directive('jxnEvent', fn($e) => '<?php echo Jaxon\\attr()->event(' .$e.'); ?>');
+        Blade::directive('jxnTarget', fn($e) => '<?php echo Jaxon\\attr()->target(' .$e.'); ?>');
 
-    public function setup(string $_ = ''): void
-    {
-        Blade::directive('jxnHtml',       fn($e) => '<?php echo Jaxon\attr()->html('       .$e.'); ?>');
-        Blade::directive('jxnBind',       fn($e) => '<?php echo Jaxon\attr()->bind('       .$e.'); ?>');
-        Blade::directive('jxnPagination', fn($e) => '<?php echo Jaxon\attr()->pagination(' .$e.'); ?>');
-        Blade::directive('jxnOn',         fn($e) => '<?php echo Jaxon\attr()->on('         .$e.'); ?>');
-        Blade::directive('jxnClick',      fn($e) => '<?php echo Jaxon\attr()->click('      .$e.'); ?>');
-        Blade::directive('jxnEvent',      fn($e) => '<?php echo Jaxon\attr()->event('      .$e.'); ?>');
-        Blade::directive('jxnTarget',     fn($e) => '<?php echo Jaxon\attr()->target('     .$e.'); ?>');
+        Blade::directive('jxnCss', fn() => '<?php echo Jaxon\\jaxon()->css(); ?>');
+        Blade::directive('jxnJs', fn() => '<?php echo Jaxon\\jaxon()->js(); ?>');
+        Blade::directive('jxnScript', fn($e) => '<?php echo Jaxon\\jaxon()->script(' .$e.'); ?>');
 
-        Blade::directive('jxnCss',    fn()    => '<?php echo Jaxon\jaxon()->css(); ?>');
-        Blade::directive('jxnJs',     fn()    => '<?php echo Jaxon\jaxon()->js(); ?>');
-        Blade::directive('jxnScript', fn($e)  => '<?php echo Jaxon\jaxon()->script(' .$e.'); ?>');
-
+        // Set up the logger and session
         $this->setLogger(Log::getLogger());
         $this->addViewRenderer('blade', '', static fn () => new View());
         $this->setSessionManager(static fn () => new Session());
 
-        if(!config('jaxon.lib.core.request.uri') && ($route = config('jaxon.app.request.route', 'jaxon')))
+        // The default request URI
+        if(!config('jaxon.lib.core.request.uri') && ($sRoute = config('jaxon.app.request.route', 'jaxon')))
         {
-            $this->uri(route($route));
+            $this->uri(route($sRoute));
         }
 
         $this->bootstrap()
@@ -55,12 +60,17 @@ class Jaxon extends App
             ->setup();
     }
 
+    /**
+     * Return the HTTP response for a Jaxon Ajax call.
+     *
+     * @param string $status
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function httpResponse(string $status = '200')
     {
-        return response(
-            $this->ajaxResponse()->getOutput(),
-            $status,
-            ['Content-Type' => $this->getContentType()]
-        );
+        return response($this->ajaxResponse()->getOutput(), $status, [
+            'Content-Type' => $this->getContentType()
+        ]);
     }
 }
--- src/JaxonServiceProvider.php
+++ src/JaxonServiceProvider.php
@@ -4,14 +4,10 @@ namespace Jaxon\Laravel;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 use Jaxon\App\AppInterface;
-use Jaxon\Config\Config;
 use Jaxon\Exception\SetupException;
 use Jaxon\Laravel\App\Jaxon as LaravelJaxon;
 use Jaxon\Laravel\Middleware\AjaxMiddleware;
 use Jaxon\Laravel\Middleware\ConfigMiddleware;
-use Jaxon\Request\Factory\RequestFactory;
-use Jaxon\Response\Manager\ResponseManager;
-
 use function config;
 use function config_path;
 use function Jaxon\jaxon;
@@ -21,39 +17,48 @@ class JaxonServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        jaxon()->di()->set(AppInterface::class, fn () => $this->app->make(LaravelJaxon::class));
+        // Provide the Jaxon\App\AppInterface
+        jaxon()->di()->set(AppInterface::class, function () {
+            return $this->app->make(LaravelJaxon::class);
+        });
 
         $this->publishes([
             __DIR__ . '/../config/config.php' => config_path('jaxon.php'),
         ], 'config');
 
+        /** @var \Illuminate\Routing\Router $router */
         $router = $this->app->make('router');
+
         $router->aliasMiddleware('jaxon.config', ConfigMiddleware::class);
         $router->aliasMiddleware('jaxon.ajax', AjaxMiddleware::class);
 
-        if (is_string($route = config('jaxon.app.request.route'))) {
-            $mw = array_unique(array_merge(
+        // Setup the route for Jaxon Ajax
+        if(($sRoute = config('jaxon.app.request.route', 'jaxon')) && is_string($sRoute))
+        {
+            $aMiddlewares = array_unique(array_merge(
                 (array)config('jaxon.app.request.middlewares', []),
                 ['jaxon.config', 'jaxon.ajax']
             ));
-            $router->post($route, fn () => response()->json([]))
-                   ->middleware($mw)
-                   ->name('jaxon');
+            $router->post($sRoute, fn () => response()->json([]))
+                ->middleware($aMiddlewares)
+                ->name('jaxon');
         }
 
-        if (config('jaxon.app.helpers.load', true)) {
+        if(config('jaxon.app.helpers.load', true))
+        {
             require_once config('jaxon.app.helpers.path', __DIR__ . '/helpers.php');
         }
     }
 
+    /**
+     * Register the single, shared Jaxon instance.
+     *
+     * @throws SetupException
+     */
     public function register(): void
     {
-        // Let Laravel build these classes
-        $this->app->bind(Config::class, fn() => new Config([]));
-        $this->app->bind(ResponseManager::class, fn() => new ResponseManager());
-        $this->app->bind(RequestFactory::class, fn() => new RequestFactory());
-
-        $this->app->singleton(LaravelJaxon::class, function(Container $app) {
+        // 1) Singleton
+        $this->app->singleton(LaravelJaxon::class, function (Container $app) {
             $jaxon = new LaravelJaxon();
             $jaxon->setup();
             return $jaxon;
